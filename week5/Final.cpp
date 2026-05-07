02ce10b9c94604d1fcce3543304feb1a8ce34f5f
Adimeow <indigojoy@live.com>
2026-05-07 10:44:38 -0500

    update for final

--- week5/Final.cpp
+++ week5/Final.cpp
@@ -1,8 +1,24 @@
 · ˚ ༘ ⋆｡˚ 𝒜𝒹𝒾 𝐹𝓁𝑜𝓇𝒶 𝒱𝒶𝓃 𝒲𝓎𝑒 ˚｡⋆ ༘ ˚ ·
-Coding 1 Final 🐉✨ -- Dragons -- ✨🐉
+Coding 1 Final 🐉✨ -- Raising Dragons -- ✨🐉
+
+#include  <iostream>
+#include <string>
+#include <vectors>
+#include <ctime>            // for random numbers
+#include <cstdlib>          // for random numbers
 
 using namespace std;
 
+class baseDragon {
+    string name
+    int health; 
+    int damage;
+
+    baseDragon() {
+        cout << "You found a baby Dragon!\n";
+        name << ""
+    }
+}
 
 int main() {
     string input;
@@ -18,7 +34,7 @@ int main() {
         } 
         // train their critter (chance to increase health or damage )
         else if(input == "train")  {
-            cout << "pump to failure, brotherrrrrrr\n";
+            cout << "Let's get stronger!\n";
         }  
         // listen to their creature (chance to increase health or damage)
         else if(input == "listen") {
