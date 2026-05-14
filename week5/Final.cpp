9608c934e8b1989e0ab8de7e490b7e6415ff45fd
Adimeow <indigojoy@live.com>
2026-05-14 11:40:19 -0500

    major fixes

--- week5/Final.cpp
+++ week5/Final.cpp
@@ -26,29 +26,50 @@ private:
 
     // constructor sets up baby dragon when it hatches!
     // a constructor function
-    babyDragon(string givenName, int giveHealth, int giveDamage) {
+    babyDragon(string givenName, int givenHealth, int givenDamage) {
         cout << "You found a baby Dragon, her name is << givenName" << "˙𐃷˙!\n";
         name = givenName;
-        health = giveHealth;
-        damage= giveDamage;
+        health = givenHealth;
+        damage= givenDamage;
         hunger = 0;
         boredom=0;
+        isAlive = true 
         status();            // 
     }
 
 
     // overload the constructor (notes from previous class)
     babyDragon() {
-        name = "Emberblossom";
+        name = "Ember";
         health = 10;
         damage = 3;
         hunger = 0;
         boredom = 0;
+        isAlive = true;
         status();
 
     }
 
-    // Learn about your baby dragon
+    // getters
+    string getName() { return name;}
+    int getHealth() { return health; }
+    int getDamage() { return damage; }
+    int getHunger() { return hunger; }
+    bool getAlive() {return isAlive; }
+
+    
+    // setters
+    void setName(string n) { name = n; }
+    void setHealth(int h) {health = h; }
+    void setDamage(int d) { damage =d; }
+    void setHunger(int h) { hunger = h;}
+    void setAlive(bool a) { isAlive = a; }
+
+
+
+
+
+    // Check on your baby dragon
     void status () {
         cout << "\n♡ " << name << ";s Stats ♡\n";
         cout << "Health : " << health << "\n";
@@ -65,22 +86,96 @@ private:
         if(hunger < 0) hunger = 0;
     }
 
-    //train your dragon
-    void train() {
-        cout << name << " trains hard and gets stronger!\n";
-        damage += 1 ;
-        hunger += 1;
-        boredom += 1;
+    //listen
+    void listen() {
+        cout << name << " makes a happy growl...\n";
+        status;
     }
 
-    // prepare for trouble!
-    void battle() {
-    
-        cout << "an enermy has appeared!\n";
-        cout << name << " attacks for " << " damage!*ੈ🐉 ༘⋆໋✧˚.\n";
-        hunger += 1;
-        boredom += 1;
+    //train your dragon
+    void train() {
+        if (hunger > 10) {
+            cout << name << " is too hungry to train.\n";
+        }
+if (hunger > 5) {
+    cout << name << " growls....baby dragon is getting hungry.\n";
+        }
+
+        cout << name << " trains hard.\n";
+
+        if (rand() % 2 ==) {
+            damage++;
+            cout << "Damage increased!\n";
+        } else {
+            health++;
+            cout << "Health increased!|n";
+
+        }
+
+        hunger++;
+        
+    }
+
+    // prepare for trouble! *ੈ🐉 ༘⋆໋✧˚
+    void battle(string enemyName, int enemyHealth, in enemyDamage) {
+        if (hunger > 10) {
+            cout << name << " is too hungry for battle.\n";
+            return;
+        }
+
+        if (hunger > 5) {
+            cout << name << " growls...is getting hungry.\n";
+        
+        }
+
+        cout << "\n A wild " <<enemyName << appears!\n;
+
+        string choice;
+
+        while (enemyHaealth > 0 && health > 0) {
+            cout << "Choose: attack / heal / run\n"
+            getline(cin, choice);
+
+            if(choice == "attacK") {
+                cout << name << " attacks for " << damage << " damage.\n";
+                enemyHealth -=damage;
+            }
+                else if (choice == "heal") {
+                cout << name << " heals.\n";
+                health += 2;
+
+                
+            }
+                else if (choice == "run") {
+                cout << name << " runs away!.\n";
+                return;
+
+            }   
+            else { 
+                cout << "Invalid choice.\n";
+                continue;
+            }         
+
+            if (enemyHealth > 0) {
+                cout << enemyName << " attack for " < enemyDamage << " damage.\n";
+                health -= enemyDamage;
+            }
+
+        }
+
+        if (health <=0 ) {
+            cout << name << " has fallen.\n";
+            isAlive = false;
+        } else {
+            cout << name << " defeated " << enemyName << "!\n";
+        }
+
+        hunger++;
+
     } 
+    
+        
+    // new day
     
     void newDay() {
         hunger += 2;
@@ -88,6 +183,21 @@ private:
     }
     
 };
+
+// load names from file
+vector<string> loadNames() {
+    vector<string> names;
+    ifstream file("name.txt");
+    string line;
+
+    while (getline(file, line)) {
+        names.push_back(line);
+    }
+
+    return names;
+
+}
+
 
 //main program -- creating dragon adventure
 int main() {
